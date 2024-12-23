e7ac94a8a447e0751b35e596aa0c7d07e8a90258
Stefan Allius <stefan.allius@t-online.de>
2024-12-23 11:06:02 +0100

    add missing run.sh

---
+++ ha_addon_debug/rootfs/run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/with-contenv bashio
+
+echo "Add-on environment started"
+
+echo "check for Home Assistant MQTT"
+MQTT_HOST=$(bashio::services mqtt "host")
+MQTT_PORT=$(bashio::services mqtt "port")
+MQTT_USER=$(bashio::services mqtt "username")
+MQTT_PASSWORD=$(bashio::services mqtt "password")
+
+# if a MQTT was/not found, drop a note
+if [ -z "$MQTT_HOST" ]; then
+    echo "MQTT not found"
+else
+    echo "MQTT found"
+    export MQTT_HOST
+    export MQTT_PORT
+    export MQTT_USER
+    export MQTT_PASSWORD
+fi
+
+
+
+
+# Create folder for log und config files
+mkdir -p /homeassistant/tsun-proxy/logs
+
+cd /home/proxy || exit
+
+export VERSION=$(cat /proxy-version.txt)
+
+echo "Start Proxyserver..."
+python3 server.py --json_config=/data/options.json  --log_path=/homeassistant/tsun-proxy/logs/ --config_path=/homeassistant/tsun-proxy/ --log_backups=2

2151340c940758b5eb50e9b7b7729c86fabaa8a5
Stefan Allius <stefan.allius@t-online.de>
2025-06-21 12:54:06 +0200

    bump to next test version

--- ha_addon_dev/rootfs/run.sh
+++ ha_addon_dev/rootfs/run.sh
@@ -15,10 +15,12 @@ if bashio::supervisor.ping; then
     else
         bashio::log.yellow "run.sh: info: Home Assistant MQTT service not available!"
     fi
+    bashio::log.yellow "run.sh: info: check for slug"
     SLUG=$(bashio::addon.repository)
     if [ -z "$SLUG" ]; then
-        bashio::log.yellow "run.sh: info: found addon slug: $SLUG"
+        bashio::log.yellow "run.sh: info1: found addon slug: $SLUG"
     fi
+    bashio::log.yellow "run.sh: info2: found addon slug: $SLUG"
 else
 
 
